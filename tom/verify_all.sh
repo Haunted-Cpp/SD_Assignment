48beddb287046a54dd3e996b941ebd4c6aa38dd6
Duarte Nóbrega <56511128+Haunted-Cpp@users.noreply.github.com>
2025-01-03 14:17:38 +0000

    added more comments to verify_all.sh

--- tom/verify_all.sh
+++ tom/verify_all.sh
@@ -1,3 +1,14 @@
+# Checks if the output files:
+
+# L802.txt 
+# L803.txt 
+# L804.txt 
+# L805.txt
+# L806.txt 
+# L807.txt
+
+# Are equal up to the maximum number of lines possible - min { cat L80i.txt | wc -l }
+
 numberMachines=6
 startIP="2"
 room="L80"
@@ -11,4 +22,5 @@ do
   number_of_lines=$(( number_of_lines == -1 || number_of_lines > lines ? lines : number_of_lines ))
 done
 
+# Calls verify.sh with the maximum number of lines possible
 bash verify.sh $number_of_lines

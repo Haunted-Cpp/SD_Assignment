1e18223c3faf00594ba07cfaac6f1790fb2f3f2d
Duarte Nóbrega <up202005727@edu.fc.up.pt>
2025-01-21 14:39:05 +0000

    updated ports again

--- p2p/runner.sh
+++ p2p/runner.sh
@@ -8,8 +8,8 @@ numberMachines=7  # Number of machines the p2p network has
 port=51243        # Port where the service will be hosted
 seconds=60        # Timeout parameter
 room="L120"       # DCC room where the processes will be hosted
-startIP="1"       # The PC hosting the first peer - L802 
-                  # In this case, the PCs "L802 L803 L804 L805 L806 L807" are used
+startIP="1"       # The PC hosting the first peer - L1201 
+                  # In this case, the PCs "L1201 L1203 L1204 L1205 L1206 L1207" are used
 folder=$(pwd)     # Folder where the files are contained
 
 # Build the `network` parameter 
@@ -41,7 +41,7 @@ do
   # Create a new window and run the command
   tmux new-window -t "$SESSION_NAME" -n "window_$i"
   nextIP=$((startIP + i))
-  tmux send-keys -t "$SESSION_NAME:$i" "clear; ssh $room$nextIP 'cd $folder; java -jar p2p.jar ${seconds} ${port} ${network[$index]}'" C-m
+  tmux send-keys -t "$SESSION_NAME:window_$i" "clear; ssh $room$nextIP 'cd $folder; java -jar p2p.jar ${seconds} ${port} ${network[$index]}'" C-m
   index=$((index + 1))
 done
 
